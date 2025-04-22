cffd05b26ce8664e11f830253cb2fba5db0b402d
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-04-22 11:39:13 +0330

    image service part1

---
+++ app/Http/Services/Image/ImageToolsService.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Http\Services\Image;
+
+class ImageToolsService
+{
+
+    protected $image;
+    protected $exclusiveDirectory;
+    protected $imageDirectory;
+    protected $imageName;
+    protected $imageFormat;
+    protected $finalImageDirectory;
+    protected $finalImageName;
+
+}
