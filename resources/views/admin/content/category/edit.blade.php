30f2c7da6fd924f9dbd76b125a92b0518d7e2ea2
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-05-13 07:56:34 +0330

    image service part9

--- resources/views/admin/content/category/edit.blade.php
+++ resources/views/admin/content/category/edit.blade.php
@@ -33,15 +33,16 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.content.category.update', $postCategory->id ) }}" method="post" enctype="multipart/form-data" id="form">
+                    <form action="{{ route('admin.content.category.update', $postCategory->id) }}" method="post"
+                        enctype="multipart/form-data" id="form">
                         @csrf
                         {{ method_field('put') }}
                         <section class="row">
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="name">نام دسته</label>
-                                    <input type="text" class="form-control form-control-sm" name="name"
-                                        id="name" value="{{ old('name', $postCategory->name) }}">
+                                    <input type="text" class="form-control form-control-sm" name="name" id="name"
+                                        value="{{ old('name', $postCategory->name) }}">
                                 </div>
                                 @error('name')
                                     <span class="alert_required text-danger p-1">
@@ -54,9 +55,9 @@
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
-                                    <input type="hidden" class="form-control form-control-sm" name="tags"
-                                        id="tags"  value="{{ old('tags', $postCategory->tags) }}">
-                                        <select class="select2 form-control form-control-sm" id="select_tags" multiple></select>
+                                    <input type="hidden" class="form-control form-control-sm" name="tags" id="tags"
+                                        value="{{ old('tags', $postCategory->tags) }}">
+                                    <select class="select2 form-control form-control-sm" id="select_tags" multiple></select>
                                 </div>
                                 @error('tags')
                                     <span class="alert_required text-danger p-1">
@@ -70,8 +71,10 @@
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select name="status" id="status" class="form-control form-control-sm">
-                                        <option value="0" @if (old('status', $postCategory->status) == 0) selected @endif>غیرفعال</option>
-                                        <option value="1" @if (old('status', $postCategory->status) == 1) selected @endif>فعال</option>
+                                        <option value="0" @if (old('status', $postCategory->status) == 0) selected @endif>غیرفعال
+                                        </option>
+                                        <option value="1" @if (old('status', $postCategory->status) == 1) selected @endif>فعال
+                                        </option>
                                     </select>
                                     @error('status')
                                         <span class="alert_required text-danger p-1">
@@ -86,7 +89,7 @@
                                 <div class="form-group">
                                     <label for="image">تصویر</label>
                                     <input type="file" class="form-control form-control-sm" name="image"
-                                        id="image" >
+                                        id="image">
                                 </div>
                                 @error('image')
                                     <span class="alert_required text-danger p-1">
@@ -95,6 +98,28 @@
                                         </strong>
                                     </span>
                                 @enderror
+
+                                <section class="row">
+                                    @php
+                                        $number = 1;
+                                    @endphp
+                                    @foreach ($postCategory->image['indexArray'] as $key => $value)
+                                        <section class="col-md-{{ 6 / $number }}">
+                                            <div class="form-check">
+                                                <input type="radio" class="form-check-input" name="currentImage"
+                                                    value="{{ $key }}" id="{{ $number }}"
+                                                    @if ($postCategory->image['currentImage'] == $key) checked @endif>
+                                                <label for="{{ $number }}" class="form-check-label mx-2">
+                                                    <img src=" {{ asset($value) }} " class="w-100" alt="">
+                                                </label>
+                                            </div>
+                                        </section>
+                                        @php
+                                            $number++;
+                                        @endphp
+                                    @endforeach
+                                </section>
+
                             </section>
                             <section class="col-12 my-3">
                                 <div class="form-group">
@@ -102,12 +127,12 @@
                                     <textarea name="description" id="description" rows="6" class="form-control form-control-sm">{{ old('description', $postCategory->description) }}</textarea>
                                 </div>
                                 @error('description')
-                                <span class="alert_required text-danger p-1">
-                                    <strong>
-                                        {{ $message }}
-                                    </strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required text-danger p-1">
+                                        <strong>
+                                            {{ $message }}
+                                        </strong>
+                                    </span>
+                                @enderror
                             </section>
                         </section>
                         <section>
@@ -125,7 +150,7 @@
     <script>
         CKEDITOR.replace('description');
     </script>
-      <script>
+    <script>
         $(document).ready(function() {
             var tags_input = $('#tags');
             var select_tags = $('#select_tags');
@@ -133,8 +158,7 @@
             var default_data = null;
 
 
-            if (tags_input.val() !=- null && tags_input.val().length > 0)
-            {
+            if (tags_input.val() != -null && tags_input.val().length > 0) {
                 default_data = default_tags.split(',');
             }
 
@@ -146,7 +170,7 @@
             });
             select_tags.children('option').attr('selected', true).trigger('change');
 
-            $('#form').submit(function( event ) {
+            $('#form').submit(function(event) {
                 if (select_tags.val() !== null && select_tags.val().length > 0) {
                     var selectedSource = select_tags.val().join(',');
                     tags_input.val(selectedSource);
