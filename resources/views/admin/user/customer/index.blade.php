c2f34ba8ce99bceecfdef0cab9036523cff9339e
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-06-23 17:01:55 +0330

    create customer user

--- resources/views/admin/user/customer/index.blade.php
+++ resources/views/admin/user/customer/index.blade.php
@@ -23,7 +23,7 @@
             <section class="main-body-container">
                 <section class="main-body-container-header">
                     <h5>
-                       مشتریان
+                        مشتریان
                     </h5>
                 </section>
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
@@ -39,31 +39,167 @@
                                 <th>#</th>
                                 <th>ایمیل</th>
                                 <th>شماره موبایل</th>
-                                <th>کد ملی</th>
                                 <th>نام</th>
                                 <th>نام خانوادگی</th>
+                                <th>نقش</th>
+                                <th>وضعیت</th>
+                                <th>فعالسازی</th>
                                 <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>m.majidi@gmail.com</td>
-                                <td>09176763254</td>
-                                <td>3380080402</td>
-                                <td>مجید</td>
-                                <td>مجیدی</td>
-                                <td class="width-22-rem text-left">
-                                    <a href="" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                    <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i> حذف</button>
-                                </td>
-                            </tr>
+                            @foreach ($users as $key => $user)
+                                <tr>
+                                    <th>{{ $key + 1 }}</th>
+                                    <td>{{ $user->email }}</td>
+                                    <td>{{ $user->mobile }}</td>
+                                    <td>{{ $user->first_name }}</td>
+                                    <td>{{ $user->last_name }}</td>
+                                    <td>سوپر ادمین</td>
+
+                                    <td>
+                                        <label class="apple-switch">
+                                            <input id="{{ $user->id }}" onchange="changeStatus({{ $user->id }})"
+                                                data-url="{{ route('admin.user.customer.status', $user->id) }}"
+                                                type="checkbox" @if ($user->status === 1) checked @endif>
+                                            <span class="apple-slider"></span>
+                                        </label>
+                                    </td>
+
+                                    <td>
+                                        <label class="apple-switch">
+                                            <input id="{{ $user->id }}-active"
+                                                onchange="changeActive({{ $user->id }})"
+                                                data-url="{{ route('admin.user.customer.activation', $user->id) }}"
+                                                type="checkbox" @if ($user->activation === 1) checked @endif>
+                                            <span class="apple-slider"></span>
+                                        </label>
+                                    </td>
+                                      <td class="width-22-rem text-left">
+                                        <a href="{{ route('admin.user.customer.edit', $user->id) }}"
+                                            class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
+                                        <form class="d-inline"
+                                            action="{{ route('admin.user.customer.destroy', $user->id) }}"
+                                            method="post">
+                                            @csrf
+                                            {{ method_field('delete') }}
+                                            <button class="btn btn-danger btn-sm delete" type="submit"><i
+                                                    class="fa fa-trash-alt"></i> حذف</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </section>
             </section>
         </section>
     </section>
+@endsection
+
+@section('script')
+    <script>
+        function changeStatus(id) {
 
 
+            var element = $("#" + id);
+
+            var url = element.attr('data-url');
+
+            var elementValue = !element.prop('checked');
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    if (response.status) {
+                        if (response.checked) {
+                            element.prop('checked', true)
+                            successToast('مشتری با موفقیت فعال شد')
+                        } else {
+                            element.prop('checked', false)
+                            successToast('مشتری با موفقیت غیرفعال شد')
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            })
+
+            function successToast(message) {
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+
+            }
+        }
+    </script>
+
+    <script>
+        function changeActive(id) {
+            var element = $("#" + id + "-active");
+            var url = element.attr('data-url');
+            var elementValue = !element.prop('checked');
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    if (response.activation) {
+                        if (response.checked) {
+                            element.prop('checked', true)
+                            successToast('فعالسازی با موفقیت انجام شد')
+                        } else {
+                            element.prop('checked', false)
+                            successToast('غیر فعالسازی با موفقیت انجام شد')
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            })
+
+            function successToast(message) {
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+
+            }
+        }
+    </script>
+
+
+    @include('admin.alerts.sweetalert.delete-confirm', ['className' => 'delete']);
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -210,16 +210,17 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
             Route::delete('/destroy/{user}', [AdminUserController::class, 'destroy'])->name('admin.user.admin-user.destroy');
             Route::get('/status/{user}', [AdminUserController::class, 'status'])->name('admin.user.admin-user.status');
             Route::get('/activation/{user}', [AdminUserController::class, 'activation'])->name('admin.user.admin-user.activation');
-
         });
         //customer
         Route::prefix('customer')->group(function () {
             Route::get('/', [CustomerController::class, 'index'])->name('admin.user.customer.index');
             Route::get('/create', [CustomerController::class, 'create'])->name('admin.user.customer.create');
             Route::post('/store', [CustomerController::class, 'store'])->name('admin.user.customer.store');
-            Route::get('/edit/{id}', [CustomerController::class, 'edit'])->name('admin.user.customer.edit');
-            Route::put('/update/{id}', [CustomerController::class, 'update'])->name('admin.user.customer.update');
-            Route::delete('/destroy/{id}', [CustomerController::class, 'destroy'])->name('admin.user.customer.destroy');
+            Route::get('/edit/{user}', [CustomerController::class, 'edit'])->name('admin.user.customer.edit');
+            Route::put('/update/{user}', [CustomerController::class, 'update'])->name('admin.user.customer.update');
+            Route::delete('/destroy/{user}', [CustomerController::class, 'destroy'])->name('admin.user.customer.destroy');
+            Route::get('/status/{user}', [CustomerController::class, 'status'])->name('admin.user.customer.status');
+            Route::get('/activation/{user}', [CustomerController::class, 'activation'])->name('admin.user.customer.activation');
         });
         //role
         Route::prefix('role')->group(function () {
@@ -271,7 +272,6 @@ Route::prefix('admin')->namespace('Admin')->group(function () {
             Route::put('/update/{sms}', [SMSController::class, 'update'])->name('admin.notify.sms.update');
             Route::delete('/destroy/{sms}', [SMSController::class, 'destroy'])->name('admin.notify.sms.destroy');
             Route::get('/status/{sms}', [SMSController::class, 'status'])->name('admin.notify.sms.status');
-
         });
     });
     //ticket
