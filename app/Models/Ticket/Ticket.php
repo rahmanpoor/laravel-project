d7096b93cbf7b5b24a4b8178965270e773d056c5
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-06-27 21:12:56 +0330

    create ticket

---
+++ app/Models/Ticket/Ticket.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models\Ticket;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Ticket extends Model
+{
+    use HasFactory, SoftDeletes;
+
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function admin(){
+        return $this->belongsTo(TicketAdmin::class, 'reference_id');
+    }
+
+    public function priority(){
+        return $this->belongsTo(TicketPriority::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(TicketCategory::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Ticket\Ticket;
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\Ticket\TicketAdmin;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -52,4 +54,14 @@ class User extends Authenticatable
     {
         return $this->first_name . " " . $this->last_name;
     }
+
+    public function ticketAdmin() {
+        return $this->hasOne(TicketAdmin::class);
+    }
+
+      public function tickets()
+     {
+         return $this->hasMany(Ticket::class);
+     }
+
 }
