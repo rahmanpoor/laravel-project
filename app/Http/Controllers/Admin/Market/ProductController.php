a50f27df88fea8bfd61049107a7256e12bdb1b02
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-07-14 14:40:33 +0330

    create market store

--- app/Http/Controllers/Admin/Market/ProductController.php
+++ app/Http/Controllers/Admin/Market/ProductController.php
@@ -130,20 +130,22 @@ class ProductController extends Controller
             }
         }
         $product->update($inputs);
-        $meta_keys = $request->meta_key;
-        $meta_values = $request->meta_value;
-        $meta_ids = array_keys($request->meta_key);
-        $metas = array_map(function ($meta_id, $meta_key, $meta_value) {
-            return array_combine(
-                ['meta_id', 'meta_key', 'meta_value'],
-                [$meta_id, $meta_key, $meta_value]
-            );
-        }, $meta_ids, $meta_keys, $meta_values);
-        foreach ($metas as $meta) {
-            ProductMeta::where('id', $meta['meta_id'])->update([
-                'meta_key' => $meta['meta_key'],
-                'meta_value' => $meta['meta_value']
-            ]);
+        if ($request->has('meta_key')) {
+            $meta_keys = $request->meta_key;
+            $meta_values = $request->meta_value;
+            $meta_ids = array_keys($request->meta_key);
+            $metas = array_map(function ($meta_id, $meta_key, $meta_value) {
+                return array_combine(
+                    ['meta_id', 'meta_key', 'meta_value'],
+                    [$meta_id, $meta_key, $meta_value]
+                );
+            }, $meta_ids, $meta_keys, $meta_values);
+            foreach ($metas as $meta) {
+                ProductMeta::where('id', $meta['meta_id'])->update([
+                    'meta_key' => $meta['meta_key'],
+                    'meta_value' => $meta['meta_value']
+                ]);
+            }
         }
         return redirect()->route('admin.market.product.index')->with('swal-success', 'محصول  شما با موفقیت ویرایش شد');
     }
@@ -157,7 +159,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-         $result = $product->delete();
+        $result = $product->delete();
         return redirect()->route('admin.market.product.index')->with('swal-success', 'محصول  شما با موفقیت حذف شد');
     }
 }
--- resources/views/admin/market/store/index.blade.php
+++ resources/views/admin/market/store/index.blade.php
@@ -23,7 +23,7 @@
             <section class="main-body-container">
                 <section class="main-body-container-header">
                     <h5>
-                       انبار
+                        انبار
                     </h5>
                 </section>
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
@@ -39,52 +39,31 @@
                                 <th>#</th>
                                 <th>نام کالا</th>
                                 <th>تصویر کالا</th>
-                                <th>موجودی</th>
-                                <th>ورودی انبار</th>
-                                <th>خروجی انبار</th>
+                                <th>تعداد قابل فروش</th>
+                                <th>تعداد رزرو شده</th>
+                                <th>تعداد فروخته شده</th>
                                 <th class="max-width-8-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th>1</th>
-                                <td>آیفون 16</td>
-                                <td><img src="{{ asset('admin-asset/images/iphone 16.jpg') }}" class="max-height-3-rem"
-                                        alt="LED"></td>
-                                <td>16</td>
-                                <td>38</td>
-                                <td>22</td>
-                                <td class="width-22-rem text-left">
-                                    <a href="{{ route('admin.market.store.add-to-store'); }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> افزایش موجودی</a>
-                                    <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i>  اصلاح موجودی</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>2</th>
-                                <td>بلندگو</td>
-                                <td><img src="{{ asset('admin-asset/images/speaker.jpg') }}" class="max-height-3-rem"
-                                        alt="LED"></td>
-                                <td>16</td>
-                                <td>38</td>
-                                <td>22</td>
-                                <td class="width-22-rem text-left">
-                                    <a href="{{ route('admin.market.store.add-to-store'); }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> افزایش موجودی</a>
-                                    <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i>  اصلاح موجودی</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>3</th>
-                                <td>LED سامسونگ</td>
-                                <td><img src="{{ asset('admin-asset/images/led.jpg') }}" class="max-height-3-rem"
-                                        alt="LED"></td>
-                                <td>16</td>
-                                <td>38</td>
-                                <td>22</td>
-                                <td class="width-22-rem text-left">
-                                    <a href="{{ route('admin.market.store.add-to-store'); }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> افزایش موجودی</a>
-                                    <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i>  اصلاح موجودی</a>
-                                </td>
-                            </tr>
+                            @foreach ($products as $product)
+                                <tr>
+                                    <th>{{ $loop->iteration }}</th>
+                                    <td>{{ $product->name }}</td>
+                                    <td><img class="max-height-3-rem"
+                                            src="{{ asset($product->image['indexArray'][$product->image['currentImage']]) }}"
+                                            alt="تصویر"></td>
+                                    <td>{{ $product->sold_number }}</td>
+                                    <td>{{ $product->frozen_number }}</td>
+                                    <td>{{ $product->marketable_number }}</td>
+                                    <td class="width-22-rem text-left">
+                                        <a href="{{ route('admin.market.store.add-to-store') }}"
+                                            class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> افزایش موجودی</a>
+                                        <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> اصلاح
+                                            موجودی</a>
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </section>
