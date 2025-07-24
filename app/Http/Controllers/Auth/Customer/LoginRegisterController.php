f4977e0d7671929baf46cdbfa8907967fd5cdb62
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-07-24 15:11:37 +0330

    recognize email or phone number

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -2,13 +2,34 @@
 
 namespace App\Http\Controllers\Auth\Customer;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\Customer\LoginRegisterRequest;
+use App\Models\User;
 
 class LoginRegisterController extends Controller
 {
     public function loginRegisterForm()
     {
         return view('customer.auth.login-register');
+    }
+
+    public function loginRegister(LoginRegisterRequest $request) {
+        $inputs = $request->all();
+
+        //check id is email or not
+        if (filter_var($inputs['id'], FILTER_VALIDATE_EMAIL)) {
+            $type = 1; // 1 => email
+            $user = User::where('email', $inputs['id'])->first();
+            if(empty($user)) {
+                $newUser['email'] = $inputs['id'];
+            }
+
+        }
+
+        //check id is phone number or not
+        elseif(preg_match('/^(\+98|98|0)9\d{9}$', $inputs['id'])) {
+            $type = 0; // 0 => phone number
+        }
     }
 }
