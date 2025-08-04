60cbaef77dca3e3e9f4cb72fa609a952302e6a1d
Abdolazim Rahmanpour <a.rahmanpour.dev@gmail.com>
2025-08-04 15:12:58 +0330

    show all banners

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -11,11 +11,6 @@
                 <section class="col-md-8 pe-md-1 ">
                     <section id="slideshow" class="owl-carousel owl-theme">
                         <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/1.jpg" alt=""></a></section>
-                        <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/2.jpg" alt=""></a></section>
-                        <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/3.jpg" alt=""></a></section>
-                        <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/4.jpg" alt=""></a></section>
-                        <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/5.jpg" alt=""></a></section>
-                        <section class="item"><a class="w-100 d-block h-auto text-decoration-none" href="#"><img class="w-100 rounded-2 d-block h-auto" src="assets/images/slideshow/6.gif" alt=""></a></section>
                     </section>
                 </section>
                 <section class="col-md-4 ps-md-1 mt-2 mt-md-0">
